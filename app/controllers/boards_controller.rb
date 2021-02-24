fe2bcb601f77e38fd3f71d4ecdfb554480399a7f
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-02-24 23:37:20 +0900

    (new, create, edit, update, destroy)アクションを定義

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,46 @@
 class BoardsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
     def index
-        @board = Board.first
+        @boards = Board.all
     end
 
     def new
+        @board = current_user.boards.build
     end
+
+    def create
+        @board = current_user.boards.build(board_params)
+        if @board.save
+            redirect_to root_path, notice: '保存できました'
+        else
+            flash.now[:error] = '保存できませんでした'
+            render :new
+        end
+    end
+
+    def edit
+        @board = current_user.boards.find(params[:id])
+    end
+
+    def update
+        @board = current_user.boards.find(params[:id])
+        if @board.update(board_params)
+            redirect_to root_path, notice: '更新できました'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
+    def destroy
+        board = current_user.boards.find(params[:id])
+        board.destroy!
+        redirect_to root_path, notice: '削除されました'
+    end
+
+    private
+        def board_params
+            params.require(:board).permit(:title, :content)
+        end
 end
