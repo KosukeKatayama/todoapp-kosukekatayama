329422da4eeb210b81e7e1eff3a04a21ca22875c
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-03-03 02:23:00 +0900

    タスクを編集できるように設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,8 +21,35 @@ class TasksController < ApplicationController
         end
     end
 
+    def edit
+        @board = Board.find(params[:board_id])
+        @task = current_user.tasks.find(params[:id])
+    end
+
+    def update
+        @board = Board.find(params[:board_id])
+        @task = current_user.tasks.find(params[:id])
+        if @task.update(task_params)
+            redirect_to board_task_path(@board, @task), notice: '更新できました'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
     private
         def task_params
             params.require(:task).permit(:title, :content, :limit_date, :eyecatch)
         end
 end
+
+# def update
+#     @board = Board.find(params[:board_id])
+#     @task = current_user.tasks.find(params[:id])
+#     if @task.update(task_params)
+#         redirect_to board_task_path(@board, @task), notice: '更新できました'
+#     else
+#         flash.now[:error] = '更新できませんでした'
+#         render :edit
+#     end
+# end
